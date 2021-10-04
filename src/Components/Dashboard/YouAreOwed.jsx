ada9caab15f30923ee83ee9203009d8960b3a2e2
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-05 02:06:55 +0530

    Card Styling Done

--- src/Components/Dashboard/YouAreOwed.jsx
+++ src/Components/Dashboard/YouAreOwed.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap'
+import '../../Styles/YouAreOwed.css'
 
 
 function YouAreOwed() {
     return (
         <div>
-            <h3 style={{margin: '15px 55px'}}>You Are Owed</h3>
-
+            <h3 style={{margin: '5px 55px'}} className="label-class">You Are Owed</h3>
+            <hr style={{width:'90%',margin: '5px 0px 15px 55px'}}></hr>
             <div className="youowe-container" style={{marginLeft: '55px'}}>
-                <Card style={{ width: '35rem', height: '19rem' }}>
+                <Card style={{ width: '35rem', height: '19rem' }} className="shadow p-3 mb-5 bg-white rounded">
 
                     <div className="youowe-info">
-                        <Card.Body className="you-owe-body" style={{ float: 'center' }}>
+                        <Card.Body className="you-owe-body text-class" style={{ float: 'center' }}>
                             Robb owe you 125$
 
                             <Button 

db83eb0078a188f765d1ce2379f8406b5e3bd790
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-05 02:09:38 +0530

    Styling and dropdown changes

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { Navbar, Nav, NavDropdown  } from 'react-bootstrap'
-import { BiUserCircle } from 'react-icons/bi'
+import { FaUserAlt } from 'react-icons/fa'
+import { MdOutlineArrowDropDown } from 'react-icons/md'
 import '../Styles/navbar.css'
 function Navigationbar() {
     return (
         <div>
-        <Navbar bg="primary" variant="dark">
+        <Navbar style={{backgroundColor:'#091130', marginBottom:'15px'}}>
             
-            <Navbar.Brand href="#home" style={{marginLeft : '25px'}}>Expensor</Navbar.Brand>
-            <BiUserCircle style={{color: 'white', size: "15rems"}}/>
-            <Nav className="me-auto">
-            <NavDropdown title="Account" id="navbarScrollingDropdown">
+            <Navbar.Brand href="#home" className="logo-title" style={{color:'white'}}>
+                Expensor
+            </Navbar.Brand>
+            <Nav className="me-auto" style={{color: 'white', size: 'lg', marginLeft:'1000px'}}>
+            <NavDropdown title="Account" style={{color: 'white'}} id="navbarScrollingDropdown">
             <NavDropdown.Item href="#">My Groups</NavDropdown.Item>
             <NavDropdown.Item href="#">My Profile</NavDropdown.Item>
-            <NavDropdown.Divider />
+            <NavDropdown.Divider/>
             <NavDropdown.Item href="#">
              Logout
             </NavDropdown.Item>
             </NavDropdown>
+            <div className="dropdown-icons" style={{cursor: 'pointer'}}>
+            <FaUserAlt style={{color: 'white', size: 'lg', marginTop:'10px'}}/>
+            <MdOutlineArrowDropDown style={{color: 'white', size: 'lg', marginTop:'10px'}}/>
+            </div>
             </Nav>
             
         </Navbar>

6b0f54adbd89569ee3c5efec2ee5ef54f60ebda2
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-05 02:05:02 +0530

    added modal to expense button

--- src/Components/Dashboard/AddExpense.jsx
+++ src/Components/Dashboard/AddExpense.jsx
@@ -6,13 +6,12 @@ import Modal from 'react-modal'
 function AddExpense() {
 
     const [show, setShow] = useState(false);
-    const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
-
+    const handleShow = () => setShow(true);
     return (
-        <div>
-            <div className="mb-2 add-exp-btn">
-            <Button variant="primary" size="lg" style={{marginTop: '18px'}} onClick={handleShow}>
+      <>  
+        <div className="mb-2 modal-btn">
+            <Button size="lg" style={{marginTop: '35px'}} onClick={(handleShow)} className="btn-block">
             Add an Expense
             </Button>
             </div>
@@ -41,11 +40,18 @@ function AddExpense() {
                     <Button variant="primary" onClick={handleClose}>Share</Button>
                 </Modal.Footer>
             </Modal>
-
-        </div>
-    )
+      </>
+    );
 }
 
 export default AddExpense
+
+
+
+
+
+
+
+
 
 

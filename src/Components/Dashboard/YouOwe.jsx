8ffb803b27e0c02706ab28ac568a770bb2f5af3a
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-05 02:07:57 +0530

    Card Styling Done

--- src/Components/Dashboard/YouOwe.jsx
+++ src/Components/Dashboard/YouOwe.jsx
@@ -4,13 +4,13 @@ import '../../Styles/YouOwe.css'
 function YouOwe() {
     return (
         <div>
-            <h3 style={{margin: '15px 55px'}}>You Owe</h3>
-
+            <h3 style={{margin: '5px 55px'}} className="label-class">You Owe</h3>
+            <hr style={{width:'90%',margin: '5px 0px 15px 55px'}}></hr>
                 <div className="youowe-container" style={{marginLeft: '55px'}}>
-                    <Card style={{ width: '35rem', height: '19rem' }}>
+                    <Card style={{ width: '35rem', height: '19rem' }} className="shadow p-3 mb-5 bg-white rounded">
 
                         <div className="youowe-info">
-                            <Card.Body className="you-owe-body" style={{ float: 'center' }}>
+                            <Card.Body className="you-owe-body text-class" style={{ float: 'center' }}>
                                 You owe 50$ to John Smith
 
                                  <Button 

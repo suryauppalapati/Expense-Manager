5bc6b7ae94355f892a9a705d04cf406565729389
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-05 02:06:28 +0530

    Card Styling Done

--- src/Components/Dashboard/Dashboard-cards.jsx
+++ src/Components/Dashboard/Dashboard-cards.jsx
@@ -7,32 +7,32 @@ import '../../Styles/Dashboard-cards.css'
 function DashboardCards() {
     return (
         <div>
-            <h2 style={{margin: '15px 55px'}}>Dashboard</h2>
+            <h2>Dashboard</h2>
+            <hr style={{width:'70%',margin: '5px 0px 5px 55px', color: '#481F90'}}></hr>
             <div className="cardContainer">
-            <Card style={{ width: '18rem'}} className="flexBlock">
+            <Card style={{ width: '18rem'}} className="flexBlock shadow p-3 mb-5 bg-white rounded">
             <Card.Body className="card-body">
-                <Card.Title>Total Balance</Card.Title>
-                <Card.Text>500$</Card.Text>
+                <Card.Title className="label-class">Total Balance</Card.Title>
+                <Card.Text className="text-class">500₹</Card.Text>
             </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }} className="flexBlock">
+            <Card style={{ width: '18rem' }} className="flexBlock shadow p-3 mb-5 bg-white rounded">
             <Card.Body className="card-body">
-                <Card.Title>You Owe</Card.Title>
-                <Card.Text>50$</Card.Text>
+                <Card.Title className="label-class">You Owe</Card.Title>
+                <Card.Text className="text-class">50₹</Card.Text>
             </Card.Body>
             </Card>
 
-            <Card style={{ width: '18rem' }} className="flexBlock">
+            <Card style={{ width: '18rem' }} className="flexBlock shadow p-3 mb-5 bg-white rounded">
             <Card.Body className="card-body">
-                <Card.Title>You are owed</Card.Title>
-                <Card.Text>125$</Card.Text>
+                <Card.Title className="label-class">You are owed</Card.Title>
+                <Card.Text className="text-class">125₹</Card.Text>
             </Card.Body>
             </Card>
 
             <AddExpense/>
             </div>
-            <hr></hr>
         </div>
     )
 }

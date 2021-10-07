02f461d40d8f21d2928755fa226ba7210d1529e5
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-07 18:21:58 +0530

    Dropdown style changed

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,12 +1,35 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 import { Navbar, Nav, NavDropdown, Modal, Alert, Button } from 'react-bootstrap'
 import { FaUserAlt } from 'react-icons/fa'
 import { BsFillBellFill } from 'react-icons/bs'
-import { MdOutlineArrowDropDown } from 'react-icons/md'
 import { MdOutlineMenuBook } from 'react-icons/md'
 import '../../Styles/navbar.css'
 
 function Navigationbar() {
+    //initialising states for Notifications
+    const [notifications, setNotifications] = useState([])
+
+    //Get Notification data
+    useEffect(() => {
+        axios.get('http://localhost:8001/get_notification')
+            .then(res => {
+                console.log(res);
+                setNotifications(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+
+    //Joining SettleNotifications and RequestNotifications
+    let settled =  notifications.settled_notification;
+    let requested = notifications.request_notification;
+
+    // // concat settled and requested
+    const allNotifications = settled + requested;
+
+    //Notification Modal states
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -17,8 +40,9 @@ function Navigationbar() {
             <Navbar.Brand href="#home" className="logo-title" style={{color:'white'}}>
                 Expense Book
             </Navbar.Brand>
-            <Nav className="me-auto" style={{color: 'white', size: 'lg', marginLeft:'970px'}}>
-            <NavDropdown style={{color: '#fff'}} title="Steve Tyson" className="dropdownTitle" id="navbarScrollingDropdown">
+            <Nav className="me-auto" style={{color: 'white', size: 'lg', marginLeft:'870px'}}>
+            <div className="username">Joseph Morgan</div>
+            <NavDropdown   title={<FaUserAlt style={{color: 'white', size: 'lg', marginTop:'-5px'}} />} className="dropdownTitle" id="navbarScrollingDropdown" >
             <NavDropdown.Item href="#">My Groups</NavDropdown.Item>
             <NavDropdown.Item href="#">My Profile</NavDropdown.Item>
             <NavDropdown.Divider/>
@@ -27,9 +51,7 @@ function Navigationbar() {
             </NavDropdown.Item>
             </NavDropdown>
             <div className="dropdown-icons" style={{cursor: 'pointer'}}>
-            <FaUserAlt style={{color: 'white', size: 'lg', marginTop:'10px'}}/>
-            <MdOutlineArrowDropDown style={{color: 'white', size: 'lg', marginTop:'10px'}}/>
-            <BsFillBellFill onClick={handleShow} style={{color: 'white', size: 'lg', marginTop:'10px', marginLeft:'5px'}}/>
+            <BsFillBellFill onClick={handleShow} size={17} style={{color: 'white', marginTop:'12px', marginLeft:'-2px'}}/>
             </div>
             </Nav>
             
@@ -39,26 +61,20 @@ function Navigationbar() {
                 </Modal.Header>
 
                 <Modal.Body>
-                        <div>
-                        <Alert variant='primary' className="alertMessage" style={{textAlign: 'center'}}>
-                            John Smith sent a request to settle his Rs.150
-                        </Alert>
-                        <Alert variant='primary' className="alertMessage" style={{textAlign: 'center'}}>
-                            Mike shared a new new expense
-                        </Alert>
+                        <div> {/*--Mapping Notification data--*/}
+                        {allNotifications && allNotifications.map(reminder => (
+                            <Alert variant='primary' className="alertMessage" style={{textAlign: 'center'}}>
+                            {reminder}
+                            </Alert>
+                        ))}  
                         </div>
+                        
                 </Modal.Body>
 
                 <Modal.Footer>
                     <Button variant="primary" style={{backgroundColor:'#091130'}} className="btn-block" onClick={handleClose}>Close</Button>
                 </Modal.Footer>
             </Modal>
-
-
-
-
-
-
 
         </Navbar>
         </div>

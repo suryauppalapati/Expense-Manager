3cb85c3b90f380833626c6fc5896bee475ab9780
suryauppalapati <suryauppalapati9@gmail.com>
2021-10-08 16:24:29 +0530

    fetching data from db failed

--- src/Components/Dashboard/YouAreOwed.jsx
+++ src/Components/Dashboard/YouAreOwed.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState, useEffect} from 'react'
 import { Card, Button } from 'react-bootstrap'
 import '../../Styles/YouAreOwed.css'
 
-
 function YouAreOwed() {
+
+    //initialising states
+    const [youAreOwed, setYouAreOwed] = useState([])
+
+    //Fetching data from db
+    useEffect(() => {
+       axios.post('http://localhost:8001/dashboard_owing_details',{"userId" : "615afedcd20a2cf1a41e37f3"})
+       .then((res) => {
+            setYouAreOwed(res.data)
+        //    console.log(res);
+       })
+       .catch((err) => {
+           console.log(err);
+       })
+    }, [])
+
     return (
         <div>
             <h3 style={{margin: '5px 55px'}} className="label-class">You are owed</h3>
@@ -11,18 +27,21 @@ function YouAreOwed() {
             <div className="youowe-container" style={{marginLeft: '55px'}} scroll>
                 <Card style={{ width: '35rem', height: '15rem' }} className="shadow p-3 mb-5 bg-white rounded">
 
-                    <div className="youowe-info">
-                        <Card.Body className="you-owe-body text-class" style={{ float: 'center' }}>
-                            Robb owe you 125Rs.
+                {youAreOwed.youAreOwedDetail && youAreOwed.youAreOwedDetail.map(data => ( 
+                            <div className="youowe-info">
+                            <Card.Body className="you-owe-body text-class" style={{ float: 'center' }}>
+                                    you owe {data.youAreOwedDetail.amount} to {data.youAreOwedDetail.you_owe_to_user_name}
 
-                            <Button className="btn-block"
-                                style={{ marginTop: '0px', marginLeft: '20px', backgroundColor:"#192b74" }} 
-                                variant="secondary" 
-                                size="sm">
-                                Request
-                            </Button>{' '}
-                    </Card.Body>
-                    </div>
+                                    <Button className="btn-block"
+                                    style={{ marginTop: '0px', marginLeft: '20px',backgroundColor:"#192b74" }} 
+                                    variant="secondary" 
+                                    size="sm">
+                                    Settle
+                                </Button>{' '}
+                        </Card.Body>
+                        </div>
+                        ))}
+                    
                 </Card>
             </div>
             <Button className="btn-settleall"
